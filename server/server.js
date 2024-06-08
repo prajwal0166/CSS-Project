fff681c7b4b2f35355c7e949bce490453e973adf
PPJWAL <you@example.com>
2024-06-08 20:24:57 +0530

    second commit

--- server/server.js
+++ server/server.js
@@ -1,31 +1,55 @@
 const express = require('express');
-const bodyparser = require('body-parser');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const api = express();
-api.use(bodyparser.json());
+
+api.use(bodyParser.json());
+
+// CORS configuration
 api.use(cors({
-    origin: '*', methods: ['GET', 'POST'], allowedHeaders: ['Content-Type', 'Authorization'], optionsSuccessStatus: 200
+    origin: '*',
+    methods: ['GET', 'POST', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    optionsSuccessStatus: 200
 }));
+
+// Logging middleware
 api.use((req, res, next) => {
     console.log(`Received ${req.method} request for ${req.url}`);
     next();
 });
+
 const port = 5500;
+
+// Handle OPTIONS requests (CORS preflight requests)
+api.options('/check-browser-version', (req, res) => {
+    res.sendStatus(200);
+});
+
+// POST endpoint to check browser version
 api.post('/check-browser-version', (req, res) => {
     const { browser, version } = req.body;
-    const filteredbrowser = browser.toLowerCase();
-    const latestversion = getlatestversion(filteredbrowser);
-    if (latestversion) {
-        const islatest = parseInt(version, 10) >= latestversion;
-        res.send({ islatest });
+    const filteredBrowser = browser.toLowerCase();
+    const latestVersion = getLatestVersion(filteredBrowser);
+    if (latestVersion) {
+        const isLatest = parseInt(version, 10) >= latestVersion;
+        res.send({ isLatest });
     } else {
-        res.status(400).json({ error: 'Unsupported browser' })
+        res.status(400).json({ error: 'Unsupported browser' });
     }
 });
-function getlatestversion(browser) {
-    const browserslist = {
-        opera: 111, chrome: 125, safari: 17, firefox: 126, msie: 11
-    }
-    return browserslist[browser];
+
+function getLatestVersion(browser) {
+    const browsersList = {
+        opera: 111,
+        chrome: 125,
+        safari: 17,
+        firefox: 126,
+        msie: 11
+    };
+    return browsersList[browser];
 }
-api.listen(port);
+
+api.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
